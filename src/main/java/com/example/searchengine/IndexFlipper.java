fab298945d66d4b58c9ed865584608d382e00cb5
redsleo02 <127219441+redsleo02@users.noreply.github.com>
2023-10-24 19:00:29 +0200

    Update IndexFlipper.java

--- src/main/java/com/example/searchengine/IndexFlipper.java
+++ src/main/java/com/example/searchengine/IndexFlipper.java
@@ -2,29 +2,53 @@ package com.example.searchengine;
 
 import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
+import com.opencsv.exceptions.CsvException;
 import org.springframework.stereotype.Component;
 
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 @Component
 public class IndexFlipper {
+    public void flipIndex(String indexFileName, String flippedIndexFileName) {
 
-    public void flipIndex(String indexFileName, String flippedIndexFileName){
-        try {
-            CSVReader csvReader = new CSVReader(new FileReader(indexFileName));
+        // Use try-with-resources to ensure proper resource management
+        try (
+                CSVReader csvReader = new CSVReader(new FileReader(indexFileName));
+                CSVWriter csvWriter = new CSVWriter(new FileWriter(flippedIndexFileName))
+        ) {
             List<String[]> csvLines = csvReader.readAll();
-            Set<String[]> lines = new HashSet<>();
-            //TODO: define lines to contain the lines that should be printed to index_flipped.csv
-            CSVWriter writer = new CSVWriter(new FileWriter(flippedIndexFileName),',', CSVWriter.NO_QUOTE_CHARACTER,' ',"\r\n");
-            for (String[] line : lines) {
-                writer.writeNext(line);
+            Map<String, Set<String>> flippedIndex = createFlippedIndex(csvLines);
+
+            // Write the flipped index to the CSV file
+            for (Map.Entry<String, Set<String>> entry : flippedIndex.entrySet()) {
+                String word = entry.getKey();
+                Set<String> references = entry.getValue();
+                String[] outputLine = new String[references.size() + 1];
+                outputLine[0] = word;
+                // Use array copying for safer and slightly optimized data insertion
+                System.arraycopy(references.toArray(), 0, outputLine, 1, references.size());
+                csvWriter.writeNext(outputLine);
             }
 
-        } catch (Exception e){
-            e.printStackTrace();
+        } catch (IOException | CsvException e) {
+            throw new RuntimeException("Error flipping index", e);
         }
-
     }
 
+    private Map<String, Set<String>> createFlippedIndex(List<String[]> csvLines) {
+        Map<String, Set<String>> flippedIndex = new HashMap<>();
+
+        for (String[] line : csvLines) {
+            String url = line[0].trim();
+
+            for (int i = 1; i < line.length; i++) {
+                String word = line[i].replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+                flippedIndex.computeIfAbsent(word, k -> new HashSet<>()).add(url);
+            }
+        }
+
+        return flippedIndex;
+    }
 }
