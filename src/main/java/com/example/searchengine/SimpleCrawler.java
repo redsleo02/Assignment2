d1655c2bb886fb874bfeb6d1c35ef2eeffa4d0eb
redsleo02 <127219441+redsleo02@users.noreply.github.com>
2023-10-24 19:03:39 +0200

    Update SimpleCrawler.java

--- src/main/java/com/example/searchengine/SimpleCrawler.java
+++ src/main/java/com/example/searchengine/SimpleCrawler.java
@@ -1,45 +1,59 @@
 package com.example.searchengine;
 
-import com.opencsv.CSVWriter;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
-import java.io.*;
-import java.util.*;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
 
 public class SimpleCrawler extends Crawler {
 
+    private static final String BASE_URL = "https://api.interactions.ics.unisg.ch/hypermedia-environment/";
 
     protected SimpleCrawler(String indexFileName) {
         super(indexFileName);
     }
 
-    public void crawl(String startUrl){
-        try {
-            int duration = 0; //TODO: update the value in the code
-            Set<String[]> lines = explore(startUrl, new HashSet<>(), new HashSet<>());
-            FileWriter fileWriter = new FileWriter(indexFileName);
-            CSVWriter writer = new CSVWriter(fileWriter,',', CSVWriter.NO_QUOTE_CHARACTER,' ',"\r\n");
-            for (String[] line : lines) {
-                writer.writeNext(line);
+    public void crawl(String startURL) {
+        long startTime = System.currentTimeMillis();
+
+        Set<String[]> lines = new HashSet<>();
+        Set<String> visited = new HashSet<>();
+        Set<String[]> fileLines = explore(startURL, lines, visited);
+
+        super.writeFile(fileLines);
+
+        long endTime = System.currentTimeMillis();
+        super.checkTime(startTime, endTime);
+    }
+
+
+    public Set<String[]> explore(String startURL, Set<String[]> lines, Set<String> visited) {
+        Queue<String> queue = new LinkedList<>();
+        queue.add(startURL);
+
+        while (!queue.isEmpty()) {
+            String currentUrl = queue.poll();
+
+            if (visited.contains(currentUrl)) continue;
+            visited.add(currentUrl);
+
+            Document doc = super.getHTML(currentUrl);
+            if (doc == null) continue;
+
+            lines.add(extracting(doc, currentUrl));
+
+            Elements links = doc.select("a[href]");
+            for (Element link : links) {
+                String absLink = link.attr("abs:href");
+                if (absLink.startsWith(BASE_URL) && !visited.contains(absLink))
+                    queue.add(absLink);
             }
-            System.out.println("duration simple crawler: "+duration);
-        } catch (Exception e){
-            e.printStackTrace();
         }
 
-    }
-
-    /**
-     *
-     * @param startUrl the url where the crawling operation starts
-     * @param lines stores the lines to print on the index file
-     * @param visited stores the urls that the program has already visited
-     * @return the set of lines to print on the index file
-     */
-    public Set<String[]> explore(String startUrl, Set<String[]> lines, Set<String> visited){
-        //TODO: complete the exploration program.
         return lines;
-
-
     }
-
 }
