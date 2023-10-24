d06133fb3a89d3cf526326d6cd045892fdaa6403
redsleo02 <127219441+redsleo02@users.noreply.github.com>
2023-10-24 18:59:36 +0200

    Update Crawler.java

--- src/main/java/com/example/searchengine/Crawler.java
+++ src/main/java/com/example/searchengine/Crawler.java
@@ -1,5 +1,6 @@
 package com.example.searchengine;
 
+import com.opencsv.CSVWriter;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -7,13 +8,17 @@ import org.jsoup.select.Elements;
 import org.springframework.stereotype.Component;
 
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+
 @Component
 public abstract class Crawler {
 
-     final String indexFileName;
+    final String indexFileName;
 
     private String baseUrl = "https://api.interactions.ics.unisg.ch/hypermedia-environment/";
 
@@ -30,6 +35,43 @@ public abstract class Crawler {
      * @param url the url where the crawling starts
      */
     public abstract void crawl(String url);
+
+    void writeFile(Set<String[]> lines) {
+        try (FileWriter fileWriter = new FileWriter(indexFileName);
+             CSVWriter writer = new CSVWriter(fileWriter, ',', CSVWriter.NO_QUOTE_CHARACTER, ' ', "\r\n")) {
+            for (String[] line : lines) {
+                writer.writeNext(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void checkTime(long startTime, long endTime) {
+        long duration = (endTime - startTime) / 1000; // Zeitdifferenz in Sekunden
+        System.out.println("duration simple crawler: " + duration + "s");
+    }
+
+
+    Document getHTML(String url) {
+        try {
+            return Jsoup.connect(url).get();
+        } catch (Exception ex) {
+            System.out.println("Error while reading url: " + url);
+            return null;
+        }
+    }
+
+    String[] extracting(Document doc, String currentUrl) {
+        Elements pTags = doc.select("p");
+        String[] line = new String[pTags.size() + 1];
+        line[0] = currentUrl.substring(currentUrl.lastIndexOf("/"));
+        int index = 1;
+        for (Element p : pTags) {
+            line[index++] = p.text();
+        }
+        return line;
+    }
 
     public  List<List<String>> getInfo(String urlString){
         List<String> keywords = new ArrayList<>();
